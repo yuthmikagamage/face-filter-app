4b43cf9af16515f875e861603227fa4c13d8f560
yuthmikagamage <vanujyuthmika@gmail.com>
2026-05-14 09:15:55 +0530

    Add face detector

--- app.js
+++ app.js
@@ -1,33 +1,69 @@
+import * as tf from "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-backend-webgl";
 
+import * as faceLandmarksDetection from "@tensorflow-models/face-landmarks-detection";
+
+await tf.setBackend("webgl");
+
 const video = document.querySelector("#video");
+const startBtn = document.querySelector("#startBtn");
 
 video.muted = true;
 video.autoplay = true;
 video.playsInline = true;
 
-const stream = await navigator.mediaDevices.getUserMedia({
-  video: {
-    facingMode: "user",
-    width: 1080,
-    height: 1080,
-  },
-  audio: false,
-});
-
-video.srcObject = stream;
-
 const streamCanvas = document.querySelector("#stream");
 
-const streamContext = streamCanvas.getContext("2d");
+const streamContext = streamCanvas.getContext("2d", {
+  willReadFrequently: true,
+});
 
 streamContext.translate(streamCanvas.width, 0);
 streamContext.scale(-1, 1);
 
-const update = () => {
+let detector;
+
+const update = async () => {
   streamContext.drawImage(video, 0, 0);
+
+  const results = await detector.estimateFaces(streamCanvas, {
+    flipHorizontal: false,
+    predictIrises: false,
+    maxFaces: 1,
+  });
+
+  if (results.length > 0) {
+    console.log("Face detected:", results[0]);
+  }
 
   requestAnimationFrame(update);
 };
 
-update();
+startBtn.addEventListener("click", async () => {
+  try {
+    detector = await faceLandmarksDetection.createDetector(
+      faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh,
+      {
+        runtime: "tfjs",
+      },
+    );
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: "user",
+        width: 1080,
+        height: 1080,
+      },
+      audio: false,
+    });
+
+    video.srcObject = stream;
+
+    startBtn.style.display = "none";
+
+    update();
+  } catch (error) {
+    console.error(error);
+    alert("Unable to start app.");
+  }
+});
